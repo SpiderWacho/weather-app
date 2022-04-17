04608a113e411c4cd9f0423e134f59250f4df9c7
SpiderWacho <gastonvecch@gmail.com>
2022-04-17 00:56:39 -0300

    -Implement search function, still has bug, when searching for invalid location it still search. -Create main temp cell and little daily cells with temp forecaste

---
+++ src/index.js
@@ -0,0 +1,240 @@
+import './style.css';
+import dayBg from './images/dayBg.jpg';
+import nightBg from './images/night.jpg';
+import clearSky from './images/sun.png';
+import thunder from './images/thunder.png'
+import snow from './images/snow.png';
+import rain from './images/rain.png';
+import cloud from './images/cloud.png';
+import logoText from './images/logo_prev_ui.png'
+import {format} from 'date-fns';
+import es from 'date-fns/locale/es'
+
+
+async function getWeather(input) {
+    let rawLocation = '';
+    try {      
+        if (input !== undefined && typeof input != 'number') {
+            try {
+                rawLocation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=f9a4a03a77656330115b00b33d0c13c2&units=metric`, {mode: 'cors'});
+            }
+            catch {
+                let newError = document.createElement('p');
+                newError.classList.add('error');
+                newError.textContent = "Location not correct";
+            }
+        }
+        else {
+            rawLocation = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Buenos+Aires&limit=5&appid=f9a4a03a77656330115b00b33d0c13c2&units=metric', {mode: 'cors'});
+        }
+        let location = await rawLocation.json();
+        let lat = location[0].lat;
+        let lon = location[0].lon;
+        let rawData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=f9a4a03a77656330115b00b33d0c13c2&units=metric`, {mode: 'cors'});
+        let data = await rawData.json();
+        if (document.querySelector('.error') != undefined) {
+            document.querySelector('.error').style.display = 'none';
+        }
+        return data;
+    }
+    catch(error) {
+        throw new Error(error);
+    }
+}
+
+const manipulateDom = (() => {
+
+    function toTitleCase(str) {
+        return str.replace(
+          /\w\S*/g,
+          function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+          }
+        );
+      }
+
+    const dataRow = document.querySelector('.dataRow');
+
+    const logoContainer = document.querySelector('.logoContainer');
+    const logo = new Image();
+    logo.classList.add('logo');
+    logo.src = logoText;
+    logoContainer.append(logo);
+    
+    function displayBackground() {
+        const dateObj = new Date();
+        const timeOfVisit = dateObj.getHours();
+    
+        if (timeOfVisit < 7 || timeOfVisit > 18  ) {
+            document.body.style.backgroundImage = `url(${nightBg})`;
+            console.log(nightBg);
+        }
+        else {
+            document.body.style.backgroundImage = `url(${dayBg}`;
+        }
+    }    
+
+    function createMainTemp(data) {
+        let container = document.querySelector('.container-main')
+        //Multiply number by 1000 to get correct date
+        console.log(data.current.dt);
+        let rawDate = new Date(data.current.dt * 1000);
+        let date = format(rawDate, 'PPP');
+
+        //Get and display min and max temp
+        let tempMax = document.createElement('p');
+        tempMax.classList.add('main-climate-day-text-tempMax');
+        tempMax.textContent = `Max: ${Math.round(data.daily[0].temp.max)}°`;
+        let tempMin = document.createElement('p');
+        tempMin.classList.add('main-climate-day-text-tempMin');
+        tempMin.textContent = `Min: ${Math.round(data.daily[0].temp.min)}°`;
+        
+        //Get date
+        let newPDate = document.createElement('p');
+        newPDate.classList.add('main-climate-day-text-date');
+        console.log(date);
+        newPDate.textContent = `${format(new Date(data.current.dt * 1000), 'PPP', {locale: es})}`;
+
+        //Get current temp
+        let feelsLike = document.createElement('p');
+        feelsLike.classList.add('feelsLike');
+        feelsLike.textContent = `${Math.round(data.current.feels_like)}° C`;
+        
+        //Get and display sunrise, sunset hours
+        let sunrise = document.createElement('p');
+        let sunset = document.createElement('p');
+        sunrise.classList.add('sunrise');
+        sunset.classList.add('sunset');
+        let sunriseHour =  format(new Date(data.current.sunrise * 1000), 'p');
+        sunrise.textContent = `Salida del sol: ${sunriseHour}`;
+        let sunsetHour =  format(new Date(data.current.sunset * 1000), 'p');
+        sunset.textContent = `Puesta del sol: ${sunsetHour}`;
+        
+        //Determine img according to weather
+        const climateIcon = new Image();
+        if (data.current.weather[0].id > 200 && data.current.weather[0].id < 300) {
+            climateIcon.src = thunder;
+        }
+        else if (data.current.weather[0].id > 500 && data.current.weather[0].id < 600) {
+            climateIcon.src = rain;
+        }
+        else if (data.current.weather[0].id > 600 && data.current.weather[0].id < 700) {
+            climateIcon.src = snow;
+        }
+        else if (data.current.weather[0].id === 800) {
+            climateIcon.src = clearSky;
+        }
+        else if (data.current.weather[0].id > 800) {
+            climateIcon.src = cloud;
+        }
+        climateIcon.classList.add('main-climate-logo');
+
+        let weather = document.createElement('p');
+        weather.classList.add('main-weather');
+        weather.textContent = `${toTitleCase(data.current.weather[0].description)}`
+
+        container.append(tempMax);
+        container.append(tempMin);
+        container.append(sunrise);
+        container.append(sunset);
+        container.append(newPDate);
+        container.append(climateIcon);
+        container.append(weather);
+        container.append(feelsLike);
+        //Modify style with js to not see the border lines while loading data
+        container.style.border = '1px solid grey';
+        container.style.boxShadow = '0px 1px 5px .3px grey';
+        dataRow.append(container);   
+    }
+
+    function createNewDiv(data) {
+        const containerDiv = document.querySelector('.container-daily');
+        for (let i = 1; i < 8; i++) {
+
+            let rawDate = new Date(data[i].dt * 1000);
+            let date = format(rawDate, 'PPP', {locale: es});
+            
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('dayDiv');
+            
+            let tempMax = document.createElement('p');
+            tempMax.classList.add('climate-day-text-temp');
+            tempMax.textContent = `Max: ${Math.round(data[i].temp.max)}°`;
+            let tempMin = document.createElement('p');
+            tempMin.classList.add('climate-day-text-tempMin');
+            tempMin.textContent = `Min: ${Math.round(data[i].temp.min)}°`;
+
+            let newPDate = document.createElement('p');
+            newPDate.classList.add('climate-day-text-date')
+            newPDate.textContent = `${date}`;
+
+            let weather = document.createElement('p');
+            weather.classList.add('weather');
+            weather.textContent = `${toTitleCase(data[i].weather[0].description)}`
+            
+            const climateIcon = new Image();
+            if (data[i].weather[0].id > 200 && data[i].weather[0].id < 300) {
+                climateIcon.src = thunder;
+            }
+            else if (data[i].weather[0].id > 500 && data[i].weather[0].id < 600) {
+                climateIcon.src = rain;
+            }
+            else if (data[i].weather[0].id > 600 && data[i].weather[0].id < 700) {
+                climateIcon.src = snow;
+            }
+            else if (data[i].weather[0].id === 800) {
+                climateIcon.src = clearSky;
+            }
+            else if (data[i].weather[0].id > 800) {
+                climateIcon.src = cloud;
+            }
+            climateIcon.classList.add('climate-logo');
+
+            newDiv.append(tempMax);
+            newDiv.append(tempMin);
+            newDiv.append(newPDate);
+            newDiv.append(weather);
+            newDiv.append(climateIcon);
+            containerDiv.append(newDiv);
+        } 
+    }
+
+    function cleanData() {
+        const containerDaily = document.querySelector('.container-daily');
+        const containerMain = document.querySelector('.container-main')
+        containerDaily.textContent = ''
+        containerMain.textContent = '';
+    }
+
+    
+    return {createNewDiv, createMainTemp, displayBackground, cleanData};
+})();
+
+manipulateDom.displayBackground();
+
+function searchCity() {
+    let input = document.querySelector('#search').value;
+    manipulateDom.cleanData();
+    populatePage(input);
+    
+
+}
+
+function populatePage(input){
+    const loader = document.querySelector('.loading');
+    function displayLoading() {
+        loader.classList.add('display'); 
+    }
+    displayLoading();
+    getWeather(input)
+    .then((data) => {manipulateDom.createNewDiv(data.daily)
+                            return data})
+    .then((data) => manipulateDom.createMainTemp(data))
+    .catch(error => {throw new Error(error)})
+    .finally(() => {loader.classList.remove("display")});
+}
+
+let searchBtn = document.querySelector('#searchBtn');
+searchBtn.addEventListener('click', searchCity);
+
+populatePage();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],  
+  },
+};
